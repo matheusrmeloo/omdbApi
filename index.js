f0b5b27bd9405c2fca62cb07355ff34c613ba81b
matheusrmeloo <matheusrodrigues030717@gmail.com>
2019-09-03 00:13:52 -0300

    Install Node Modules

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 
 app.use(express.json());
 
+app.use(function(req, res, next){
+    console.log(`logging...`);
+});
+
 
 
 app.post(`/filme`, (req, res) => {
@@ -14,9 +18,24 @@ app.get(`/filme`, (req, res) => {
 });
 
 app.get(`/filme/:id`, (req, res) => {
-    res.send('HelloWorld');
+    const filme = courses.find(c => c.id === parseInt(req.params.id));
+    if(!filme) res.status(404).send('Id não encontrado');
+    else res.send(filme);
 });
 
+app.delete(`/api/courses/:id`, (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course){ 
+        return res.status(404).send('Id não encontrado');
+    };
+
+    const index = courses.indexOf(course);
+    courses.splice(index,1);
+
+    res.send(course);
+    console.log(course);
+    console.log(courses);
+});
 
 
 
