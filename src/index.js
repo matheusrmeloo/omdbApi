f8e730f41091b85eaa2fa24559a6f05d027637d2
matheusrmeloo <matheusrodrigues030717@gmail.com>
2019-09-05 16:23:13 -0300

    Adding schema

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+
+// http://www.omdbapi.com/?i=tt3896198&apikey=147b5624
  
 mongoose.connect('mongodb://127.0.0.1:27017/OMDB', {useNewUrlParser: true});
 
