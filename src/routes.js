6816fdc34b9ddf99cd0aad31cbdcfc23c08b8313
matheusrmeloo <matheusrodrigues030717@gmail.com>
2019-09-07 14:08:08 -0300

    Editing Schema

--- src/routes.js
+++ src/routes.js
@@ -6,23 +6,7 @@ const routes = express.Router();
 
 const OmdbController = require (`./controllers/OmdbController`);
 
-// app.post(`/filme`, (req, res) => {
-//     res.send('HelloWorld');
-// });
-
 routes.get(`/filme`, OmdbController.index);
 routes.post(`/filme`, OmdbController.store);
-
-// app.get(`/filme/:id`, (req, res) => {
-//     const filme = courses.find(c => c.id === parseInt(req.params.id));
-//     if(!filme) res.status(404).send('Id não encontrado');
-//     else res.send(filme);
-// });
-
-// app.delete(`/api/courses/:id`, (req, res) => {
-//     const course = courses.find(c => c.id === parseInt(req.params.id));
-//     if (!course){ 
-//         return res.status(404).send('Id não encontrado');
-//     };
 
 module.exports = routes;
